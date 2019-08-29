bc860a5d0c2fbaa698b1ff547e21622da8cbbd17
bimal125 <pandeybi125@gmail.com>
2019-08-29 17:33:04 +0545

    postgress database setup

--- school_mgnt/settings.py
+++ school_mgnt/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
+DEBUG = env('DEBUG')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'school_mgnt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': env('DBNAME'),
+        'USER': env('DBUSER'),
+        'PASSWORD': env('DBPASSWORD'),
+        'HOST': env('DBHOST'),   # Or an IP Address that your DB is hosted on
+        'PORT': '',
     }
 }
 
