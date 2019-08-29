9167511cd9d12ca5916cba48e72940ec35e4a764
bimal125 <pandeybi125@gmail.com>
2019-08-29 17:49:58 +0545

    School model created

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+  name = models.CharField(max_length=20)
+  max_students = models.PositiveIntegerField(null=True,blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
