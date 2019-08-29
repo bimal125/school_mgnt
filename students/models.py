cdc922f1c0348c16c76dd4a67d849928d9994ebd
bimal125 <pandeybi125@gmail.com>
2019-08-29 17:58:08 +0545

    student modle with migration created

--- students/models.py
+++ students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from schools.models import School
 # Create your models here.
+class Student(models.Model):
+  fname = models.CharField(max_length=20)
+  lname = models.CharField(max_length=20)
+  roll = models.CharField(max_length=20, unique=True)
+  school = models.ForeignKey(School, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
