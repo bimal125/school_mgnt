0674a5b612535723faf02a6f27aefe1924049fc7
bimal125 <pandeybi125@gmail.com>
2019-08-30 10:44:31 +0545

    documentation url  added

--- school_mgnt/urls.py
+++ school_mgnt/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
+from rest_framework.documentation import include_docs_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
 
 urlpatterns = [
+    url(r'^docs/', include_docs_urls(title='School Management',
+                                    authentication_classes=[],
+                                    permission_classes=[permissions.AllowAny,])),
     path('admin/', admin.site.urls),
     path('api/v1/schools/', include('schools.urls')),
     path('api/v1/students/', include('students.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
