7931141e50b2a2adf8c6110891c032ea844b87ad
bimal125 <pandeybi125@gmail.com>
2019-08-31 22:57:39 +0545

    static dir removed

--- school_mgnt/settings.py
+++ school_mgnt/settings.py
@@ -148,9 +148,9 @@ STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
